1b831ea0f172a986c0bfb0d067733839ead4dc1b
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-15 16:50:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,16 +4,20 @@ class Artist
 
   @@all = []
 
-  def self.all
-    @@all
-  end
-
-
   def initialize(name)
     @name = name
     @songs = []
 
   end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
 
 
 
