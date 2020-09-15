795d4b6781b71bffb723b0f83afaae75873b9a66
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-15 17:00:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,44 +10,20 @@ class Artist
     @@all
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   def add_song(song)
     @songs << song
   end
-
-
 
   def self.find_or_create_by_name(name)
 
   end
 
-
-
   def print_songs
     puts @songs.collect {|x| x.name}
-
   end
-
-
-
 
   def self.all
     @@all
-
-
   end
 
 
