d60c46d37a1f433a5d7e8fcbeb4860bc5ff68467
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-15 16:37:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,11 +23,7 @@ class Artist
 
 
   def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+  
   end
 
   def self.create(name)
